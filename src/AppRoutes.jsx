fb54b24630437184a1c61057a359f15f4ea07c5d
Yuhang Zhang <yzhang43@usc.edu>
2025-12-22 18:59:16 -0800

    HP-16 Create Login page

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router';
 import Register from './components/Register';
 import Home from './components/Home';
+import Login from './components/Login';
 
 function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ function AppRoutes() {
         <Route path=":something?" element={<Home />} />
       </Route>
       <Route path="register?" element={<Register />} />
+      <Route path="login?" element={<Login />} />
     </Routes>
   );
 }
