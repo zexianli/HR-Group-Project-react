e34a3e61e8e39154019e12d0f32d34234545a5e2
Zexian <zexianlie@gmail.com>
2025-12-28 19:09:20 -0800

    redirect from home to personal page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,7 @@
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
-import ProtectedRoute from '../components/ProtectedRoute';
+import { Navigate } from 'react-router';
 
 function Home() {
-  // login
-  // already logged in: go to personal
-  // to do this, useEffect
-  // in the useEffect, check user and token (to know whether they are logged in or not)
-  return (
-    <ProtectedRoute>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: '100vw',
-          height: '100vh',
-        }}
-      >
-        <CircularProgress size={100} />
-        {/* Hello World */}
-      </Box>
-    </ProtectedRoute>
-  );
+  return <Navigate to="/personal" replace />;
 }
 
 export default Home;
